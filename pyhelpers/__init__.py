9d84ed6054ff566b99082cdac3a5b476dab66843
mikeqfu <qian.fu@outlook.com>
2019-03-05 17:29:09 +0000

    Modify __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -1,5 +1,6 @@
 from pyhelpers.directory import *
 from pyhelpers.download import *
 from pyhelpers.geometry import *
+from pyhelpers.misc import *
 from pyhelpers.storage import *
 from pyhelpers.text import *
