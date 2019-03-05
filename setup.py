3a3e399d05f135d9adaad773361a169a0466a7a1
mikeqfu <qian.fu@outlook.com>
2019-03-05 15:20:13 +0000

    Update setup.py and README.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as readme:
 setuptools.setup(
 
     name='pyhelpers',
-    version='1.0.0',
+    version='1.0.1',
 
     author='Qian Fu',
     author_email='qian.fu@outlook.com',
