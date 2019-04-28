4c72fdb10cadf7762d521b5f299a655838ca021f
mikeqfu <qian.fu@outlook.com>
2019-04-28 16:17:34 +0100

    Slightly modified function cd()

--- pyhelpers/directory.py
+++ pyhelpers/directory.py
@@ -5,18 +5,19 @@ import os
 import pkg_resources
 
 
-# Change directory and sub-directories
-def cd(*directories):
-    # Current working directory
-    path = os.getcwd()
-    for directory in directories:
-        path = os.path.join(path, directory)
+# Change directory
+def cd(*sub_dir, mkdir=True):
+    path = os.getcwd()  # Current working directory
+    for d in sub_dir:
+        path = os.path.join(path, d)
+    if mkdir:
+        os.makedirs(path, exist_ok=True)
     return path
 
 
 # Change directory to "dat" and sub-directories
-def cdd(*directories):
+def cdd(*sub_dir):
     path = pkg_resources.resource_filename(__name__, 'dat/')
-    for directory in directories:
+    for directory in sub_dir:
         path = os.path.join(path, directory)
     return path
