1ebb28567661b2d1e71b9219411bc4e4ac6d1250
mikeqfu <qian.fu@outlook.com>
2019-05-31 22:16:59 +0100

    Modified download()

--- pyhelpers/download.py
+++ pyhelpers/download.py
@@ -1,6 +1,8 @@
 """ Download files """
 
 import os
+import time
+import urllib.request
 
 import progressbar
 import requests
@@ -8,30 +10,52 @@ import tqdm
 
 
 # Download and show progress
-def download(url, path_to_file):
+def download(url, path_to_file, wait_to_retry=3600, show_progress=True):
     """
-
     Reference: https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
-
-    :param url:
-    :param path_to_file:
+    :param url: [str]
+    :param path_to_file: [str]
+    :param wait_to_retry: [int; float]
+    :param show_progress: [bool]
     :return:
     """
-    r = requests.get(url, stream=True)  # Streaming, so we can iterate over the response
-    total_size = int(r.headers.get('content-length'))  # Total size in bytes
-    block_size = 1024 * 1024
-    wrote = 0
 
     directory = os.path.dirname(path_to_file)
     if not os.path.exists(directory):
         os.mkdir(directory)
 
-    with open(path_to_file, 'wb') as f:
-        for data in tqdm.tqdm(r.iter_content(block_size), total=total_size // block_size, unit='MB'):
-            wrote = wrote + len(data)
-            f.write(data)
-    if total_size != 0 and wrote != total_size:
-        print("ERROR, something went wrong")
+    if show_progress:
+        # Using 'Requests'
+        r = requests.get(url, stream=True)  # Streaming, so we can iterate over the response
+
+        if r.status_code == 429:
+            time.sleep(wait_to_retry)
+
+        total_size = int(r.headers.get('content-length'))  # Total size in bytes
+        if total_size == 0:
+            print("ERROR! Something went wrong")
+        block_size = 1024 * 1024
+        block_no = total_size // block_size
+
+        wrote = 0
+        with open(path_to_file, 'wb') as f:
+            for data in tqdm.tqdm(r.iter_content(block_size), total=block_no, unit='MB'):
+                wrote = wrote + len(data)
+                f.write(data)
+            f.close()
+
+        if wrote != total_size:
+            print("ERROR! Something went wrong")
+
+        r.close()
+
+    else:
+        print("\nDownloading \"{}\" ... ".format(os.path.basename(path_to_file)), end="")
+        try:
+            urllib.request.urlretrieve(url, path_to_file)
+            print("\nDone.")
+        except Exception as e:
+            print("\nFailed. {}".format(e))
 
 
 # Make a custom bar to show downloading progress
@@ -50,8 +74,8 @@ def make_custom_progressbar():
     return progress_bar
 
 
-#
-def show_progress(block_count, block_size, total_size):
+# Show downloading progress
+def show_progress_bar(block_count, block_size, total_size):
     p_bar = make_custom_progressbar()
     if p_bar.max_value is None:
         p_bar.max_value = total_size
