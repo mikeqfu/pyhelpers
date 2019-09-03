7bd584935012d1f109e35de5d6820cd5571f30eb
mikeqfu <qian.fu@outlook.com>
2019-09-03 10:57:15 +0100

    Replace mkdir with makedirs

--- pyhelpers/download.py
+++ pyhelpers/download.py
@@ -7,11 +7,12 @@ import time
 # Download and show progress
 def download(url, path_to_file, wait_to_retry=3600):
     """
-    Reference: https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
     :param url: [str]
     :param path_to_file: [str]
     :param wait_to_retry: [int; float]
-    :return:
+
+    Reference: https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
+
     """
     import requests
     r = requests.get(url, stream=True)  # Streaming, so we can iterate over the response
@@ -25,14 +26,13 @@ def download(url, path_to_file, wait_to_retry=3600):
 
     directory = os.path.dirname(path_to_file)
     if not os.path.exists(directory):
-        os.mkdir(directory)
+        os.makedirs(directory)
 
     import tqdm
     with open(path_to_file, 'wb') as f:
         for data in tqdm.tqdm(r.iter_content(block_size), total=total_size // block_size, unit='MB'):
             wrote = wrote + len(data)
             f.write(data)
-
     f.close()
 
     r.close()
