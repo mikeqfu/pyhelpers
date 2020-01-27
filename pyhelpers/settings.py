0d8a461b89f0d8a917e52a491c17e8f6d0cbf3cf
mikeqfu <qian.fu@outlook.com>
2020-01-27 17:52:22 +0000

    modified gdal_configurations()

--- pyhelpers/settings.py
+++ pyhelpers/settings.py
@@ -60,19 +60,19 @@ def mpl_preferences(reset=False, font_name=None):
 
 
 # Set GDAL configurations
-def gdal_configurations(reset=False, max_tmpfile_size=2500):
+def gdal_configurations(reset=False, max_tmpfile_size=5000):
     import gdal
     if not reset:
         # Whether to enable interleaved reading. Defaults to NO.
         gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'YES')
         # Whether to enable custom indexing. Defaults to YES.
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
+        gdal.SetConfigOption('OSM_USE_CUSTOM_INDEXING', 'YES')
         # Whether to compress nodes in temporary DB. Defaults to NO.
-        gdal.SetConfigOption('COMPRESS_NODES', 'YES')
+        gdal.SetConfigOption('OSM_COMPRESS_NODES', 'YES')
         # Maximum size in MB of in-memory temporary file. If it exceeds that value, it will go to disk. Defaults to 100.
-        gdal.SetConfigOption('MAX_TMPFILE_SIZE', str(max_tmpfile_size))
+        gdal.SetConfigOption('OSM_MAX_TMPFILE_SIZE', str(max_tmpfile_size))
     else:
         gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'NO')
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
-        gdal.SetConfigOption('COMPRESS_NODES', 'NO')
-        gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
+        gdal.SetConfigOption('OSM_USE_CUSTOM_INDEXING', 'YES')
+        gdal.SetConfigOption('OSM_COMPRESS_NODES', 'NO')
+        gdal.SetConfigOption('OSM_MAX_TMPFILE_SIZE', '100')
