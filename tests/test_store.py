7b6a647bc7d672c604306bb335b783663b6d0b9d
mikeqfu <qian.fu@outlook.com>
2020-05-30 20:18:11 +0100

    updated test_store.py

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,10 +1,7 @@
 # store.py
 
-import numpy as np
-import pandas as pd
-
 from pyhelpers.dir import cd
-from pyhelpers.store import load_feather, load_pickle, save_feather, save_pickle
+from pyhelpers.store import *
 
 xy_array = np.array([(530034, 180381),   # London
                      (406689, 286822),   # Birmingham
@@ -12,8 +9,8 @@ xy_array = np.array([(530034, 180381),   # London
                      (582044, 152953)])  # Leeds
 dat = pd.DataFrame(xy_array, columns=['Easting', 'Northing'])
 
-path_to_test_pickle = cd("tests", "data", "dat.pickle")  # cd("tests\\data\\dat.pickle")
-print(path_to_test_pickle)  # >>> C:\Users\fuq\DataShare\Experiments\pyhelpers\tests\data\dat.pickle
+path_to_test_pickle = cd("tests", "data", "dat.pickle")  # path_to_test_pickle == cd("tests\\data\\dat.pickle")
+print(path_to_test_pickle)  # C:\Users\fuq\DataShare\Experiments\pyhelpers\tests\data\dat.pickle
 
 # Save dat as a pickle file
 save_pickle(dat, path_to_test_pickle, verbose=True)
