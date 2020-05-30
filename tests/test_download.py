69a516afebed7b5db9d2339505f31669ab5a4c37
mikeqfu <qian.fu@outlook.com>
2020-05-30 20:18:00 +0100

    updated test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,7 +6,7 @@ from pyhelpers.dir import cd
 from pyhelpers.download import download
 
 url = 'https://www.python.org/static/community_logos/python-logo-master-v3-TM.png'
-path_to_python_logo = cd("tests", "picture", "python-logo.png")
+path_to_python_logo = cd("tests", "images", "python-logo.png")
 download(url, path_to_python_logo)
 
 path_to_python_logo = "python-logo.png"
