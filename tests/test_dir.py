f1f6391adfe02240da3224f2e980774fe2bf2b0d
mikeqfu <qian.fu@outlook.com>
2020-06-22 14:14:29 +0100

    Remove tests/

--- tests/test_dir.py
+++
@@ -1,9 +0,0 @@
-# dir.py
-
-from pyhelpers.dir import cd
-
-path_to_pickle = cd("tests", "dat.pickle")
-print(path_to_pickle)
-
-path_to_test_pickle = cd("tests\\data", "dat.pickle")  # cd("tests\\data\\dat.pickle")
-print(path_to_test_pickle)
