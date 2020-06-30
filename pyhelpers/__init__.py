5ef9b2c8ae336a87b61988b86a58371334a90293
mikeqfu <qian.fu@outlook.com>
2020-06-30 20:48:18 +0100

    Modified __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -1,6 +1,6 @@
 from pyhelpers import dir, geom, ops, settings, sql, store, text
 
-__pkgname__ = 'pyhelpers'
+__package_name__ = 'pyhelpers'
 __author__ = 'Qian Fu'
 __version__ = '1.2.0'
 __email__ = 'qian.fu@outlook.com'
