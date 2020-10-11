dd8c4b2c55a9b9db7807f29834c79a5d69b9e025
mikeqfu <qian.fu@outlook.com>
2020-10-11 18:06:06 +0100

    modify tests\test_ops.py

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -3,26 +3,7 @@ import matplotlib.pyplot as plt
 from pyhelpers.ops import *
 
 
-def test_fake_requests_headers():
-    fake_header = fake_requests_headers()
-    print(fake_header)
-    # {'User-Agent': '<str>'}
-
-    random = True
-    fake_header = fake_requests_headers(random)
-    print(fake_header)
-    # {'User-Agent': '<str>'}
-
-
-def test_download_file_from_url():
-    url = 'https://www.python.org/static/community_logos/python-logo-master-v3-TM.png'
-
-    from pyhelpers.dir import cd
-
-    path_to_file = cd("tests\\images", "python-logo.png")
-
-    download_file_from_url(url, path_to_file)
-
+# For general use
 
 def test_confirmed():
     prompt = "Create Directory?"
@@ -33,39 +14,7 @@ def test_confirmed():
     # True
 
 
-def test_get_variable_name():
-    x = 1
-    var_str = get_variable_name(x)
-    print(var_str)
-    # 'x'
-
-    y = 2
-    var_str = get_variable_name(y)
-    print(var_str)
-    # y
-
-    y = x
-    var_str = get_variable_name(y)
-    print(var_str)
-    # x
-
-
-def test_get_variable_names():
-    x = 1
-    var_name = get_variable_names(x)
-    print(list(var_name))
-    # ['x']
-
-    y = 2
-    var_names = get_variable_names(x, y)
-    print(list(var_names))
-    # ['x', 'y']
-
-    y = 1
-    var_names = get_variable_names(x, y)
-    print(list(var_names))
-    # ['x', 'x']
-
+# For iterable manipulation
 
 def test_split_list_by_size():
     lst = list(range(0, 10))
@@ -172,6 +121,65 @@ def test_remove_multiple_keys_from_dict():
     # {'k2': 'v2', 'k5': 'v5'}
 
 
+# Tabular data
+
+def test_detect_nan_for_str_column():
+    data_frame = pd.DataFrame(np.resize(range(10), (10, 2)), columns=['a', 'b'])
+    data_frame.iloc[3, 1] = np.nan
+
+    nan_col_pos = detect_nan_for_str_column(data_frame, column_names=None)
+    print(list(nan_col_pos))
+    # [1]
+
+
+def test_create_rotation_matrix():
+    theta = 30
+
+    rotation_mat = create_rotation_matrix(theta)
+    print(rotation_mat)
+    # [[-0.98803162  0.15425145]
+    #  [-0.15425145 -0.98803162]]
+
+
+def test_dict_to_dataframe():
+    input_dict = {'a': 1, 'b': 2}
+
+    data_frame = dict_to_dataframe(input_dict)
+    print(data_frame)
+    #   key  value
+    # 0   a      1
+    # 1   b      2
+
+
+def test_parse_csr_matrix():
+    import scipy.sparse
+    from pyhelpers.dir import cd
+
+    indptr = np.array([0, 2, 3, 6])
+    indices = np.array([0, 2, 2, 0, 1, 2])
+    data = np.array([1, 2, 3, 4, 5, 6])
+    csr_m = scipy.sparse.csr_matrix((data, indices, indptr), shape=(3, 3))
+
+    path_to_csr = cd("tests\\data", "csr_mat.npz")
+    np.savez_compressed(path_to_csr,
+                        indptr=csr_m.indptr,
+                        indices=csr_m.indices,
+                        data=csr_m.data,
+                        shape=csr_m.shape)
+
+    csr_mat = parse_csr_matrix(path_to_csr, verbose=True)
+    # Loading "..\\tests\\data\\csr_mat.npz" ... Done.
+
+    # .nnz gets the count of explicitly-stored values (non-zeros)
+    print((csr_mat != csr_m).count_nonzero() == 0)
+    # True
+
+    print((csr_mat != csr_m).nnz == 0)
+    # True
+
+
+# For simple computation
+
 def test_get_extreme_outlier_bounds():
     num_dat = pd.DataFrame(range(100), columns=['col'])
 
@@ -207,6 +215,8 @@ def test_find_closest_date():
     print(closest_date)
     # 2019-01-02 00:00:00.000000
 
+
+# For graph plotting
 
 def test_cmap_discretisation():
     import matplotlib.cm
@@ -257,109 +267,88 @@ def test_colour_bar_index():
     print("Done.")
 
 
-def test_detect_nan_for_str_column():
-    data_frame = pd.DataFrame(np.resize(range(10), (10, 2)), columns=['a', 'b'])
-    data_frame.iloc[3, 1] = np.nan
+# For web scraping
 
-    nan_col_pos = detect_nan_for_str_column(data_frame, column_names=None)
-    print(list(nan_col_pos))
-    # [1]
+def test_fake_requests_headers():
+    fake_header = fake_requests_headers()
+    print(fake_header)
+    # {'User-Agent': '<str>'}
 
-
-def test_create_rotation_matrix():
-    theta = 30
-
-    rotation_mat = create_rotation_matrix(theta)
-    print(rotation_mat)
-    # [[-0.98803162  0.15425145]
-    #  [-0.15425145 -0.98803162]]
+    random = True
+    fake_header = fake_requests_headers(random)
+    print(fake_header)
+    # {'User-Agent': '<str>'}
 
 
-def test_dict_to_dataframe():
-    input_dict = {'a': 1, 'b': 2}
+def test_download_file_from_url():
+    url = 'https://www.python.org/static/community_logos/python-logo-master-v3-TM.png'
 
-    data_frame = dict_to_dataframe(input_dict)
-    print(data_frame)
-    #   key  value
-    # 0   a      1
-    # 1   b      2
+    from pyhelpers.dir import cd
+
+    path_to_file = cd("tests\\images", "python-logo.png")
+
+    download_file_from_url(url, path_to_file)
 
 
 if __name__ == '__main__':
-    print("\nTesting 'fake_requests_headers()':")
-
-    test_fake_requests_headers()
-
-    print("\nTesting 'download_file_from_url()':")
-
-    test_download_file_from_url()
-
+    # For general use
     print("\nTesting 'confirmed()':")
-
     test_confirmed()
 
-    print("\nTesting 'get_variable_name()':")
-
-    test_get_variable_name()
-
-    print("\nTesting 'get_variable_names()':")
-
-    test_get_variable_names()
-
+    # For iterable manipulation
     print("\nTesting 'split_list_by_size()':")
-
     test_split_list_by_size()
 
     print("\nTesting 'split_list()':")
-
     test_split_list()
 
     print("\nTesting 'split_iterable()':")
-
     test_split_iterable()
 
     print("\nTesting 'update_nested_dict()':")
-
     test_update_nested_dict()
 
     print("\nTesting 'get_all_values_from_nested_dict()':")
-
     test_get_all_values_from_nested_dict()
 
     print("\nTesting 'remove_multiple_keys_from_dict()':")
-
     test_remove_multiple_keys_from_dict()
 
-    print("\nTesting 'get_extreme_outlier_bounds()':")
+    # Tabular data
+    print("\nTesting 'detect_nan_for_str_column()':")
+    test_detect_nan_for_str_column()
 
+    print("\nTesting 'create_rotation_matrix()':")
+    test_create_rotation_matrix()
+
+    print("\nTesting 'dict_to_dataframe()':")
+    test_dict_to_dataframe()
+
+    print("\nTesting 'load_csr_matrix()':")
+    test_parse_csr_matrix()
+
+    # For simple computation
+    print("\nTesting 'get_extreme_outlier_bounds()':")
     test_get_extreme_outlier_bounds()
 
     print("\nTesting 'interquartile_range()':")
-
     test_interquartile_range()
 
     print("\nTesting 'find_closest_date()':")
-
     test_find_closest_date()
 
+    # For graph plotting
     print("\nTesting 'cmap_discretisation()':")
-
     test_cmap_discretisation()
 
     print("\nTesting 'colour_bar_index()':")
-
     test_colour_bar_index()
 
     plt.show()
 
-    print("\nTesting 'detect_nan_for_str_column()':")
+    # For web scraping
+    print("\nTesting 'fake_requests_headers()':")
+    test_fake_requests_headers()
 
-    test_detect_nan_for_str_column()
-
-    print("\nTesting 'create_rotation_matrix()':")
-
-    test_create_rotation_matrix()
-
-    print("\nTesting 'dict_to_dataframe()':")
-
-    test_dict_to_dataframe()
+    print("\nTesting 'download_file_from_url()':")
+    test_download_file_from_url()
