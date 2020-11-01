1a488cc3b3b73c8f308e2011fec4c323d4273f1b
mikeqfu <qian.fu@outlook.com>
2020-11-01 12:52:25 +0000

    suppress inspection for unused import statements in __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -1,15 +1,22 @@
+# noinspection PyUnresolvedReferences
 from .dir import *
+# noinspection PyUnresolvedReferences
 from .geom import *
+# noinspection PyUnresolvedReferences
 from .ops import *
+# noinspection PyUnresolvedReferences
 from .settings import *
+# noinspection PyUnresolvedReferences
 from .sql import *
+# noinspection PyUnresolvedReferences
 from .store import *
+# noinspection PyUnresolvedReferences
 from .text import *
 
 __package_name__ = 'pyhelpers'
 __package_name_alt__ = 'PyHelpers'
 __author__ = u'Qian Fu'
-__version__ = '1.2.5'
+__version__ = '1.2.6'
 __email__ = 'qian.fu@outlook.com'
 __description__ = \
     'A handy toolkit for facilitating Python users\' data manipulation tasks.'
