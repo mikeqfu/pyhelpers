77252b610eb8e7b49ba99e55d9c9e00b88e09cdb
mikeqfu <qian.fu@outlook.com>
2020-11-18 10:49:43 +0000

    modify __description__ in __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -18,4 +18,4 @@ __package_name_alt__ = 'PyHelpers'
 __author__ = u'Qian Fu'
 __version__ = '1.2.7'
 __email__ = 'qian.fu@outlook.com'
-__description__ = "A handy toolkit for facilitating Python users' data manipulation tasks."
+__description__ = "An open-source toolkit for facilitating Python users' data manipulation tasks."
