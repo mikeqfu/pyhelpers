46bed9836b4a5a0505dc7a64438915aa6ff143ea
mikeqfu <qian.fu@outlook.com>
2021-04-13 11:56:40 +0100

    Modify __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -16,7 +16,7 @@ from .text import *
 __package_name__ = 'pyhelpers'
 __package_name_alt__ = 'PyHelpers'
 __author__ = 'Qian Fu'
-__version__ = '1.2.15rc1'
+__version__ = '1.2.15rc2'
 __email__ = 'qian.fu@outlook.com'
 __description__ = \
     "PyHelpers: an open-source toolkit for facilitating Python users' data manipulation tasks."
