8c0bf88c659e2f92d02af101d883ca0c921cc185
mikeqfu <qian.fu@outlook.com>
2021-04-19 00:02:23 +0100

    Update __version__ in __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -16,7 +16,7 @@ from .text import *
 __package_name__ = 'pyhelpers'
 __package_name_alt__ = 'PyHelpers'
 __author__ = 'Qian Fu'
-__version__ = '1.2.15rc3'
+__version__ = '1.2.15'
 __email__ = 'qian.fu@outlook.com'
 __description__ = \
     "PyHelpers: an open-source toolkit for facilitating Python users' data manipulation tasks."
