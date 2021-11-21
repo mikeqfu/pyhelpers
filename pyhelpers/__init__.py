b1535de2aeda9badfd690289173ab191cc91e3a3
mikeqfu <qian.fu@outlook.com>
2021-11-21 08:09:10 +0000

    Modify __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -1,12 +1,20 @@
 import datetime
+import json
 
-__project__ = 'PyHelpers'
-__package__ = 'pyhelpers'
-__description__ = "An open-source toolkit for facilitating Python users' data manipulation tasks."
+import pkg_resources
 
-__author__ = 'Qian Fu'
-__affiliation__ = 'School of Engineering, University of Birmingham'
-__email__ = 'q.fu@bham.ac.uk'
+f = open(pkg_resources.resource_filename(__name__, "dat\\metadata"), mode='r')
+metadata = json.load(f)
+f.close()
+
+__project__ = metadata['Project']
+__package__ = metadata['Package']
+
+__author__ = metadata['Author']
+__affiliation__ = metadata['Affiliation']
+__email__ = metadata['Email']
+
+__description__ = metadata['Description']
 
 __copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
 
