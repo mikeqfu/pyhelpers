8a3643e2c450c19563b90350eb0423973c81cd77
mikeqfu <qian.fu@outlook.com>
2021-11-23 16:47:13 +0000

    Modify __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -3,19 +3,19 @@ import json
 
 import pkg_resources
 
-f = open(pkg_resources.resource_filename(__name__, "dat\\metadata"), mode='r')
-metadata = json.load(f)
-f.close()
+with open(pkg_resources.resource_filename(__name__, "dat\\metadata"), mode='r') as metadata_file:
+    metadata = json.load(metadata_file)
 
 __project__ = metadata['Project']
 __package__ = metadata['Package']
 
 __author__ = metadata['Author']
 __affiliation__ = metadata['Affiliation']
-__email__ = metadata['Email']
+__author_email__ = metadata['Email']
 
 __description__ = metadata['Description']
 
 __copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
 
-__version__ = '1.3.0a4'
+__version__ = metadata['Version']
+__license__ = metadata['License']
