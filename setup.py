194d9ab812ba67e5bed250d099a97e4c2c946a97
mikeqfu <qian.fu@outlook.com>
2022-02-10 10:28:38 +0000

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,69 +1,30 @@
+import json
+
 import setuptools
 
-from pyhelpers import __author__, __author_email__, __description__, __license__, __package__, \
-    __version__
+with open("src/pyhelpers/data/metadata.json", mode='r') as metadata_file:
+    metadata = json.load(metadata_file)
 
-with open("README.rst", mode='r', encoding='utf-8') as readme:
-    long_description = readme.read()
+__package__ = metadata['Package']
+__author__ = metadata['Author']
+__author_email__ = metadata['Email']
+__description__ = metadata['Description']
+__version__ = metadata['Version']
+__license__ = metadata['License']
+
+__home_page__ = 'https://github.com/mikeqfu/' + f'{__package__}'
 
 setuptools.setup(
-
     name=__package__,
-
     version=__version__,
-
     description=__description__,
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-
-    url=f'https://github.com/mikeqfu/{__package__}',
-
+    url=__home_page__,
     author=__author__,
     author_email=__author_email__,
-
     license=__license__,
-
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-
-        'Topic :: Education',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Utilities',
-
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-
-        'Programming Language :: Python :: 3',
-
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-    ],
-
-    keywords=['Python', 'Utilities', 'Helpers'],
-
     project_urls={
         'Documentation': f'https://{__package__}.readthedocs.io/en/{__version__}/',
-        'Source': f'https://github.com/mikeqfu/{__package__}',
-        'Tracker': f'https://github.com/mikeqfu/{__package__}/issues',
+        'Source': __home_page__,
+        'Tracker': __home_page__ + f'/issues',
     },
-
-    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
-
-    install_requires=[
-        'numpy',
-        'pyproj',
-        'scipy',
-        'Shapely',
-        'pandas',
-        'requests',
-        'SQLAlchemy',
-        'psycopg2',
-    ],
-
-    package_data={"": ["dat/*"]},
-    include_package_data=True,
-
 )
