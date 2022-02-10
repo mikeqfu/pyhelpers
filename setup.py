85fc6d6c0ba1c75a060359b9fe857e20222a458b
mikeqfu <qian.fu@outlook.com>
2022-02-10 15:49:24 +0000

    Modify setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,18 @@ import setuptools
 with open("pyhelpers/data/metadata.json", mode='r') as metadata_file:
     metadata = json.load(metadata_file)
 
-__package__ = metadata['Package']
-__author__ = metadata['Author']
-__author_email__ = metadata['Email']
-__description__ = metadata['Description']
-__version__ = metadata['Version']
-__license__ = metadata['License']
+__package__, __version__ = metadata['Package'], metadata['Version']
 
 __home_page__ = 'https://github.com/mikeqfu/' + f'{__package__}'
 
 setuptools.setup(
     name=__package__,
     version=__version__,
-    description=__description__,
+    description=metadata['Description'],
     url=__home_page__,
-    author=__author__,
-    author_email=__author_email__,
-    license=__license__,
+    author=metadata['Author'],
+    author_email=metadata['Email'],
+    license=metadata['License'],
     project_urls={
         'Documentation': f'https://{__package__}.readthedocs.io/en/{__version__}/',
         'Source': __home_page__,
