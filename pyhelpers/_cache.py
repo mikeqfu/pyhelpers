6c07b8879af58315fa8cc0747e7c0852eee1b6ef
Qian Fu <q.fu@bham.ac.uk>
2024-07-16 14:26:19 +0100

    Improve the docstrings of `_cache.py`

--- pyhelpers/_cache.py
+++ pyhelpers/_cache.py
@@ -17,23 +17,27 @@ _OPTIONAL_DEPENDENCY = json.loads(
 
 def _confirmed(prompt=None, confirmation_required=True, resp=False):
     """
-    Type to confirm whether to proceed or not.
+    Prompt the user for confirmation to proceed.
+
+    This function prompts the user with a Yes/No message specified by ``prompt``.
+    If ``confirmation_required=True``, the user must confirm to proceed.
+    The function returns ``True`` if the user confirms (either by typing ``'y'`` or ``'yes'``),
+    otherwise ``False``.
 
     See also [`OPS-C-1 <https://code.activestate.com/recipes/541096/>`_].
 
-    :param prompt: a message that prompts a response (Yes/No), defaults to ``None``
+    :param prompt: Message prompting a response (Yes/No); defaults to ``None``.
     :type prompt: str | None
-    :param confirmation_required: whether to require users to confirm and proceed, defaults to ``True``
+    :param confirmation_required: Whether confirmation is mandatory; defaults to ``True``.
     :type confirmation_required: bool
-    :param resp: default response, defaults to ``False``
+    :param resp: Default response if no input is provided; defaults to ``False``.
     :type resp: bool
-    :return: a response
+    :return: ``True`` if confirmed, ``False`` otherwise.
     :rtype: bool
 
     **Tests**::
 
         >>> from pyhelpers._cache import _confirmed
-
         >>> if _confirmed(prompt="Testing if the function works?", resp=True):
         ...     print("Passed.")
         Testing if the function works? [Yes]|No: yes
@@ -47,9 +51,9 @@ def _confirmed(prompt=None, confirmation_required=True, resp=False):
             prompt_ = copy.copy(prompt)
 
         if resp is True:  # meaning that default response is True
-            prompt_ = "{} [{}]|{}: ".format(prompt_, "Yes", "No")
+            prompt_ = f"{prompt_} [Yes]|No: "
         else:
-            prompt_ = "{} [{}]|{}: ".format(prompt_, "No", "Yes")
+            prompt_ = f"{prompt_} [No]|Yes: "
 
         ans = input(prompt_)
         if not ans:
@@ -66,30 +70,30 @@ def _confirmed(prompt=None, confirmation_required=True, resp=False):
 
 def _check_dependency(name, package=None):
     """
-    Import optional dependency package.
+    Import an optional dependency package/module.
 
-    :param name: name of a package/module as an optional dependency of pyhelpers
+    Attempts to import the module specified by ``name`` as an optional dependency.
+    If ``package`` is provided, attempts to import ``name`` from within ``package``.
+
+    :param name: Name of the package/module as an optional dependency of pyhelpers.
     :type name: str
-    :param package: [optional] name of a package that contains the module specified by ``name``,
-        defaults to ``None``
-    :type package: str or None
+    :param package: Name of the package that contains ``name``; defaults to ``None``.
+    :type package: str | None
+    :return: Imported module object.
+    :rtype: module
 
     **Tests**::
 
         >>> from pyhelpers._cache import _check_dependency
-
         >>> psycopg2_ = _check_dependency(name='psycopg2')
         >>> psycopg2_.__name__
         'psycopg2'
-
         >>> pyodbc_ = _check_dependency(name='pyodbc')
         >>> pyodbc_.__name__
         'pyodbc'
-
         >>> sqlalchemy_dialects = _check_dependency(name='dialects', package='sqlalchemy')
         >>> sqlalchemy_dialects.__name__
         'sqlalchemy.dialects'
-
         >>> gdal_ = _check_dependency(name='gdal', package='osgeo')
         >>> gdal_.__name__
         'osgeo.gdal'
@@ -130,19 +134,22 @@ def _check_dependency(name, package=None):
 
 def _check_rel_pathname(pathname):
     """
-    Check for the relative pathname (to the current working directory).
+    Check if the pathname is relative to the current working directory.
 
-    :param pathname: A pathname (of a file or a directory).
+    This function returns a relative pathname of the input `pathname` to the current working
+    directory if ``pathname`` is within the current working directory;
+    otherwise, it returns a copy of the input.
+
+    :param pathname: Pathname (of a file or directory).
     :type pathname: str | bytes | pathlib.Path
-    :return: A relative pathname of the input ``pathname`` to the current working directory
-        if it is within the current working directory; otherwise, a copy of the input.
+    :return: A relative pathname of ``pathname`` to the current working directory
+        if it is within the current working directory; otherwise, a copy of ``pathname``.
     :rtype: str
 
     **Tests**::
 
         >>> from pyhelpers._cache import _check_rel_pathname
         >>> from pyhelpers.dirs import cd
-
         >>> _check_rel_pathname(".")
         '.'
         >>> _check_rel_pathname(cd())
@@ -166,31 +173,30 @@ def _check_rel_pathname(pathname):
 
 def _check_file_pathname(name, options=None, target=None):
     """
-    Check pathname of a specified file given its name or filename.
+    Check the pathname of a specified file given its name or filename.
 
-    :param name: name or filename of the application that is to be called
+    This function determines whether the specified executable file exists and returns its pathname.
+
+    :param name: Name or filename of the executable file.
     :type name: str
-    :param options: possible pathnames or directories, defaults to ``None``
+    :param options: Possible pathnames or directories to search for ``name``; defaults to ``None``.
     :type options: list | set | None
-    :param target: specific pathname (that may be known already), defaults to ``None``
+    :param target: Specific pathname that may already be known; defaults to ``None``.
     :type target: str | None
-    :return: whether the specified executable file exists and its pathname
+    :return: Tuple containing a boolean indicating if the file exists and its pathname.
     :rtype: tuple[bool, str]
 
     **Tests**::
 
         >>> from pyhelpers._cache import _check_file_pathname
         >>> import os
-
         >>> python_exe = "python.exe"
         >>> python_exe_exists, path_to_python_exe = _check_file_pathname(python_exe)
         >>> python_exe_exists
         True
-
         >>> possible_paths = [
         ...     "C:\\Program Files\\Python310",
         ...     "C:\\Program Files\\Python310\\python.exe"]
-
         >>> python_exe_exists, path_to_python_exe = _check_file_pathname(
         ...     python_exe, target=possible_paths[0])
         >>> python_exe_exists
@@ -202,7 +208,6 @@ def _check_file_pathname(name, options=None, target=None):
         >>> python_exe_exists, path_to_python_exe = _check_file_pathname(possible_paths[1])
         >>> python_exe_exists
         True
-
         >>> text_exe = "pyhelpers.exe"  # This file does not actually exist
         >>> test_exe_exists, path_to_test_exe = _check_file_pathname(text_exe, possible_paths)
         >>> test_exe_exists
@@ -247,20 +252,23 @@ def _format_err_msg(e=None, msg=""):
     """
     Format an error message.
 
-    :param e: Exception or any of its subclasses, defaults to ``None``
+    This function formats an error message by combining ``msg`` and ``e`` (if provided).
+    If ``e`` is provided and is a string, it appends it to ``msg``.
+    If ``e`` is an ``Exception`` or its subclass, it includes the exception's message
+    in the final output.
+
+    :param e: Exception or any of its subclasses; defaults to ``None``.
     :type e: Exception | BaseException | str | None
-    :param msg: default error message, defaults to ``""``
+    :param msg: Default error message; defaults to ``""``.
     :type msg: str
-    :return: an error message
+    :return: Formatted error message.
     :rtype: str
 
     **Tests**::
 
         >>> from pyhelpers._cache import _format_err_msg
-
         >>> _format_err_msg("test")
         'test.'
-
         >>> _format_err_msg("test", msg="Failed.")
         'Failed. test.'
     """
@@ -278,15 +286,28 @@ def _format_err_msg(e=None, msg=""):
 
 
 def _print_failure_msg(e, msg="Failed.", verbose=True):
+    # noinspection PyShadowingNames
     """
-    Print the error message associated with occurrence of an``Exception``.
+    Print an error message associated with the occurrence of an ``Exception``.
 
-    :param e: Exception or any of its subclasses, defaults to ``None``
+    Prints the error message ``msg`` along with details of ``e`` (if provided).
+    If ``verbose=True``, additional relevant information is printed to the console.
+
+    :param e: ``Exception`` or any of its subclasses; defaults to ``None``.
     :type e: Exception | BaseException | str | None
-    :param msg: default error message, defaults to ``"Failed."``
+    :param msg: Default error message to print; defaults to ``"Failed."``.
     :type msg: str
-    :param verbose: whether to print relevant information in console, defaults to ``True``
+    :param verbose: Whether to print additional information to the console; defaults to ``True``.
     :type verbose: bool | int
+
+    **Tests**::
+
+        >>> from pyhelpers._cache import _print_failure_msg
+        >>> try:
+        ...     result = 1 / 0  # This will raise a ZeroDivisionError
+        ... except ZeroDivisionError as e:
+        ...     _print_failure_msg(e, msg="Error:")
+        Error: division by zero.
     """
 
     err_msg = _format_err_msg(e=e, msg=msg)
@@ -299,15 +320,17 @@ def example_dataframe(osgb36=False):
     """
     Create an example dataframe.
 
-    :param osgb36: whether to use data based on OSGB36 National Grid, defaults to ``True``
+    This functions creates and returns a pandas DataFrame with geographical coordinates either in
+    OSGB36 National Grid format (default) or in longitude and latitude format.
+
+    :param osgb36: Whether to use data based on OSGB36 National Grid; defaults to ``True``.
     :type osgb36: bool
-    :return: an example dataframe
+    :return: An example dataframe with geographical coordinates.
     :rtype: pandas.DataFrame
 
     **Tests**::
 
         >>> from pyhelpers._cache import example_dataframe
-
         >>> example_dataframe()
                           Easting       Northing
         City
@@ -315,7 +338,6 @@ def example_dataframe(osgb36=False):
         Birmingham  406705.887014  286868.166642
         Manchester  383830.039036  398113.055831
         Leeds       430147.447354  433553.327117
-
         >>> example_dataframe(osgb36=False)
                     Longitude   Latitude
         City
