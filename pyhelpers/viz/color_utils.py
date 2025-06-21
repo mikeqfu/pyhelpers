b3ef582b9b184a635e7a5374681d85ddb6974f79
Qian Fu <q.fu@bham.ac.uk>
2025-06-21 11:26:39 +0100

    Refactor the submodule: `colour_utils` → `color_utils`

--- pyhelpers/viz/color_utils.py
+++ pyhelpers/viz/color_utils.py
@@ -7,7 +7,7 @@ import numpy as np
 from .._cache import _check_dependencies
 
 
-def cmap_discretisation(cmap, n_colours):
+def cmap_discretization(cmap, n_colors):
     # noinspection PyShadowingNames
     """
     Creates a discrete colormap based on the input.
@@ -20,8 +20,8 @@ def cmap_discretisation(cmap, n_colours):
     :type cmap:
         matplotlib.colors.ListedColormap | matplotlib.colors.LinearSegmentedColormap |
         matplotlib.colors.Colormap | str
-    :param n_colours: Number of colors to discretize the colormap.
-    :type n_colours: int
+    :param n_colors: Number of colors to discretize the colormap.
+    :type n_colors: int
     :return: A discrete colormap derived from the continuous ``cmap``.
     :rtype: matplotlib.colors.LinearSegmentedColormap
 
@@ -32,16 +32,15 @@ def cmap_discretisation(cmap, n_colours):
 
     **Examples**::
 
-        >>> from pyhelpers.viz import cmap_discretisation
+        >>> from pyhelpers.viz import cmap_discretization
         >>> from pyhelpers.settings import mpl_preferences
-        >>> import matplotlib
         >>> import matplotlib.pyplot as plt
         >>> import numpy as np
         >>> mpl_preferences()
-        >>> cm_accent = cmap_discretisation(cmap=matplotlib.colormaps['Accent'], n_colours=5)
+        >>> cm_accent = cmap_discretization(cmap=plt.colormaps['Accent'], n_colors=5)
         >>> cm_accent.name
         'Accent_5'
-        >>> cm_accent = cmap_discretisation(cmap='Accent', n_colours=5)
+        >>> cm_accent = cmap_discretization(cmap='Accent', n_colors=5)
         >>> cm_accent.name
         'Accent_5'
         >>> fig = plt.figure(figsize=(10, 2), constrained_layout=True)
@@ -72,44 +71,44 @@ def cmap_discretisation(cmap, n_colours):
     else:
         cmap_ = cmap
 
-    colours_ = np.concatenate((np.linspace(0, 1., n_colours), (0., 0., 0., 0.)))
+    colors_ = np.concatenate((np.linspace(0, 1., n_colors), (0., 0., 0., 0.)))
     # noinspection PyTypeChecker
-    colours_rgba = cmap_(colours_)
-    indices = np.linspace(0, 1., n_colours + 1)
+    colors_rgba = cmap_(colors_)
+    indices = np.linspace(0, 1., n_colors + 1)
     c_dict = {}
 
     for ki, key in enumerate(('red', 'green', 'blue')):
         c_dict[key] = [
-            (indices[x], colours_rgba[x - 1, ki], colours_rgba[x, ki])
-            for x in range(n_colours + 1)]
+            (indices[x], colors_rgba[x - 1, ki], colors_rgba[x, ki])
+            for x in range(n_colors + 1)]
 
-    colour_map = mpl_colors.LinearSegmentedColormap(cmap_.name + '_%d' % n_colours, c_dict, 1024)
+    color_map = mpl_colors.LinearSegmentedColormap(cmap_.name + '_%d' % n_colors, c_dict, 1024)
 
-    return colour_map
+    return color_map
 
 
-def colour_bar_index(cmap, n_colours, labels=None, **kwargs):
+def color_bar_index(cmap, n_colors, labels=None, **kwargs):
     """
-    Creates a colour bar with correctly aligned labels.
+    Creates a color bar with correctly aligned labels.
 
     .. note::
 
-        - To avoid off-by-one errors, this function takes a standard colour ramp,
-          discretizes it, and then draws a colour bar with labels aligned correctly.
-        - See also [`OPS-CBI-1
+        - To avoid off-by-one errors, this function takes a standard colormap,
+          discretizes it, and then draws a color bar with labels aligned correctly.
+        - See also [`VIZ-CBI-1
           <https://sensitivecities.com/so-youd-like-to-make-a-map-using-python-EN.html
           #.WbpP0T6GNQB>`_].
 
     :param cmap: A colormap instance,
         e.g. built-in `colormaps`_ accessible via `matplotlib.cm.get_cmap()`_.
     :type cmap: matplotlib.colors.ListedColormap | matplotlib.colors.Colormap
-    :param n_colours: Number of discrete colours to use in the colour bar.
-    :type n_colours: int
-    :param labels: Optional list of labels for the colour bar; defaults to ``None``.
+    :param n_colors: Number of discrete colors to use in the color bar.
+    :type n_colors: int
+    :param labels: Optional list of labels for the color bar; defaults to ``None``.
     :type labels: list | None
-    :param kwargs: [Optional] Additional optional parameters for the funtion
-        `matplotlib.pyplot.colorbar()`_.
-    :return: A colour bar object.
+    :param kwargs: [Optional] Additional optional parameters for the
+        `matplotlib.pyplot.colorbar()`_ function.
+    :return: A color bar object.
     :rtype: matplotlib.colorbar.Colorbar
 
     .. _`colormaps`:
@@ -121,13 +120,13 @@ def colour_bar_index(cmap, n_colours, labels=None, **kwargs):
 
     **Examples**::
 
-        >>> from pyhelpers.viz import colour_bar_index
+        >>> from pyhelpers.viz import color_bar_index
         >>> from pyhelpers.settings import mpl_preferences
         >>> import matplotlib.pyplot as plt
         >>> mpl_preferences()
         >>> fig1 = plt.figure(figsize=(2, 6), constrained_layout=True)
         >>> ax1 = fig1.add_subplot()
-        >>> cbar1 = colour_bar_index(cmap=plt.colormaps['Accent'], n_colours=5)
+        >>> cbar1 = color_bar_index(cmap=plt.colormaps['Accent'], n_colors=5)
         >>> ax1.tick_params(axis='both', which='major', labelsize=14)
         >>> cbar1.ax.tick_params(labelsize=14)
         >>> # ax.axis('off')
@@ -151,7 +150,7 @@ def colour_bar_index(cmap, n_colours, labels=None, **kwargs):
         >>> fig2 = plt.figure(figsize=(2, 6), constrained_layout=True)
         >>> ax2 = fig2.add_subplot()
         >>> labels_ = list('abcde')
-        >>> cbar2 = colour_bar_index(cmap=plt.colormaps['Accent'], n_colours=5, labels=labels_)
+        >>> cbar2 = color_bar_index(cmap=plt.colormaps['Accent'], n_colors=5, labels=labels_)
         >>> ax2.tick_params(axis='both', which='major', labelsize=14)
         >>> cbar2.ax.tick_params(labelsize=14)
         >>> # ax.axis('off')
@@ -172,18 +171,17 @@ def colour_bar_index(cmap, n_colours, labels=None, **kwargs):
 
     mpl_cm, mpl_plt = _check_dependencies('matplotlib.cm', 'matplotlib.pyplot')
 
-    # assert isinstance(cmap, mpl_cm.ListedColormap)
-    cmap_ = cmap_discretisation(cmap, n_colours)
+    cmap_ = cmap_discretization(cmap, n_colors)
 
     mappable = mpl_cm.ScalarMappable(cmap=cmap_)
     mappable.set_array(np.array([]))
-    mappable.set_clim(-0.5, n_colours + 0.5)
+    mappable.set_clim(-0.5, n_colors + 0.5)
 
-    colour_bar = mpl_plt.colorbar(mappable=mappable, ax=mpl_plt.gca(), **kwargs)
-    colour_bar.set_ticks(np.linspace(0, n_colours, n_colours))
-    colour_bar.set_ticklabels(range(n_colours))
+    color_bar = mpl_plt.colorbar(mappable=mappable, ax=mpl_plt.gca(), **kwargs)
+    color_bar.set_ticks(np.linspace(0, n_colors, n_colors))
+    color_bar.set_ticklabels(range(n_colors))
 
     if labels:
-        colour_bar.set_ticklabels(labels)
+        color_bar.set_ticklabels(labels)
 
-    return colour_bar
+    return color_bar
