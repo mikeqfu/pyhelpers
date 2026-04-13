06ef44a2d9eaf858b670a3717c3333af67e095bb
Qian Fu <q.fu@bham.ac.uk>
2026-04-13 11:42:20 +0100

    chore: refine copyright string format in __init__.py

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -24,7 +24,7 @@ __email__ = metadata['Email']
 
 __version__ = metadata['Version']
 __license__ = metadata['License']
-__copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
+__copyright__ = f'2019-{datetime.datetime.now().year} {__author__}'
 
 __first_release__ = metadata['First release']
 
